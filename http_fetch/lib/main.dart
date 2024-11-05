aa17f7d296cba3ec1d2592465b22ff57cf723415
InkySaaS <saraurruzola@gmail.com>
2024-11-05 13:57:42 -0300

    yes

---
+++ http_fetch/lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: UsersScreen(),
+    );
+  }
+}
+
+class UsersScreen extends StatefulWidget {
+  @override
+  _UsersScreenState createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  late Future<List<User>> _futureUsers;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureUsers = fetchUsers();
+  }
+
+  Future<List<User>> fetchUsers() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((user) => User.fromJson(user)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+      ),
+      body: FutureBuilder<List<User>>(
+        future: _futureUsers,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index].name),
+                  subtitle: Text(snapshot.data![index].email),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text('Nenhum usuário encontrado.'));
+          }
+        },
+      ),
+    );
+  }
+}
+
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+    );
+  }
+}
